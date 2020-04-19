8f114394abec6a0114faea193e165fed235dbfa9
Bhuvana <bhuvaneswari.thangamani@cognizant.com>
2020-04-19 20:44:17 +0530

    Eureka Changes Commit

--- applications/backlog-server/src/main/java/io/pivotal/pal/tracker/backlog/App.java
+++ applications/backlog-server/src/main/java/io/pivotal/pal/tracker/backlog/App.java
@@ -1,4 +1,5 @@
 package io.pivotal.pal.tracker.backlog;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
@@ -11,6 +12,7 @@ import java.util.TimeZone;
 
 
 @SpringBootApplication
+@EnableEurekaClient
 @ComponentScan({"io.pivotal.pal.tracker.backlog", "io.pivotal.pal.tracker.restsupport"})
 public class App {
 
