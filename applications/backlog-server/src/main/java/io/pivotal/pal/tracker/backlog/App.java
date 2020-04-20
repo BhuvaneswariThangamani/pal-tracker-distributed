ecd2b7d2091be40b019820643e6a5be8edddc29e
Bhuvana <bhuvaneswari.thangamani@cognizant.com>
2020-04-20 20:15:20 +0530

    distributed system

--- applications/backlog-server/src/main/java/io/pivotal/pal/tracker/backlog/App.java
+++ applications/backlog-server/src/main/java/io/pivotal/pal/tracker/backlog/App.java
@@ -1,9 +1,9 @@
 package io.pivotal.pal.tracker.backlog;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -14,12 +14,12 @@ import org.springframework.web.client.RestOperations;
 import java.util.TimeZone;
 
 
+@EnableWebSecurity
+@EnableResourceServer
+@EnableOAuth2Client
 @SpringBootApplication
 @EnableEurekaClient
 @ComponentScan({"io.pivotal.pal.tracker.backlog", "io.pivotal.pal.tracker.restsupport"})
-@EnableResourceServer
-@EnableWebSecurity
-@EnableOAuth2Client
 public class App {
 
     public static void main(String[] args) {
--- applications/timesheets-server/src/main/java/io/pivotal/pal/tracker/timesheets/App.java
+++ applications/timesheets-server/src/main/java/io/pivotal/pal/tracker/timesheets/App.java
@@ -3,23 +3,22 @@ package io.pivotal.pal.tracker.timesheets;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.web.client.RestOperations;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 import java.util.TimeZone;
 
-
+@EnableWebSecurity
+@EnableResourceServer
+@EnableOAuth2Client
 @SpringBootApplication
 @EnableEurekaClient
 @ComponentScan({"io.pivotal.pal.tracker.timesheets", "io.pivotal.pal.tracker.restsupport"})
-@EnableResourceServer
-@EnableWebSecurity
-@EnableOAuth2Client
 public class App {
 
     public static void main(String[] args) {
